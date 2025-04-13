e6af4580ecabcc89727acce98cb2d2c601e077e2
vadym-usk <vadym.usk@gmail.com>
2025-04-13 13:34:01 +0300

    fixed allure reports for 2 browsers

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,7 @@
 import './commands';
 import '@shelex/cypress-allure-plugin';
 
-before(() => {
-  const browser = Cypress.browser?.name || 'unknown';
-  cy.allure().parameter('Browser', browser);
+beforeEach(() => {
+  const browserName = Cypress.browser.name;
+  cy.allure().tag(`browser: ${browserName}`);
 });
